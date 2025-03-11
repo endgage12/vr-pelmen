e79d49c11db660c9de9c2893b49a405061b69b2f
alexandr <endgage12@gmail.com>
2025-03-11 15:08:10 +0300

    for test purposes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -40,126 +40,21 @@
 
     <a-box position="0 1 -5" ammo-body="type: dynamic" ammo-shape="type: box"></a-box>
 
-    <a-camera>
-      <a-cursor></a-cursor>
-    </a-camera>
-
-    <!-- Player. -->
-    <a-entity id="cameraRig">
+    <!-- Игровой риг с камерой и контроллерами -->
+    <a-entity id="rig">
+      <a-camera id="camera"></a-camera>
       <a-entity
-        id="camera"
-        position="0 1.6 0.5"
-        camera="far: 10000"
-        look-controls
-        player-height
-        wasd-controls="acceleration: 15"
+        laser-controls="hand: left; model: true;"
+        raycaster="near: 0.1; far: 3; objects: .clickable"
       ></a-entity>
       <a-entity
-        id="cameraWallCollider"
-        follow-position="target: #camera"
-        raycaster__wall="objects: [data-wall-active]; interval: 150; direction: 0 -1 0; far: 5"
-        visible="false"
+        laser-controls="hand: right; model: true;"
+        raycaster="near: 0.1; far: 3; objects: .clickable"
       ></a-entity>
     </a-entity>
 
-    <a-entity
-      id="controllerRig"
-      proxy-event="event: recentered; to: #cameraRig; captureBubbles: true; as: recenter"
-    >
-      <a-entity
-        id="leftHand"
-        class="weapon"
-        bind__hand-swapper="enabled: {{ otherHand }}RaycasterActive"
-        bind__haptics-wall="enabled: isPlaying && gameMode === 'classic'"
-        bind__headfist="isPlaying: isPlaying"
-        bind__menu-controls="enabled: mainMenuActive"
-        bind__pauser="enabled: isPlaying"
-        bind__punch="enabled: isPlaying && gameMode === 'punch'"
-        bind__raycaster="enabled: leftRaycasterActive"
-        bind__raycaster__game="enabled: isPlaying && (gameMode === 'classic' || gameMode === 'punch')"
-        bind__blade="enabled: isPlaying && gameMode === 'classic'"
-        bind__weapon-particles="enabled: isPlaying; gameMode: gameMode"
-        bind__trail="colorScheme: colorScheme; enabled: isPlaying && gameMode === 'classic'"
-        controller="hand: left"
-        data-hand="left"
-        haptics="events: mouseenter; dur: 35; force: 0.075"
-        haptics__beat="events: beathaptic; dur: 90; force: 0.55"
-        haptics__draw="events: drawblade; dur: 750; force: 0.025"
-        haptics__plume="events: plumepulse; dur: 35; force: 0.1"
-        headfist="hand: left"
-        raycaster="objects: [raycastable]; far: 5; showLine: false"
-        raycaster__game="objects: [raycastable-game]; far: 1; interval: {{ hand == 'right' and '70' or '71' }}; direction: 0 1 -1"
-        weapon="hand: left"
-        weapon-particles="hand: left"
-        thumb-controls
-        thumb-controls-debug="enabled: false; hand: left; controllerType: meta-controls"
-        render-order="weapon"
-        trail="color: secondary; hand: left"
-      >
-        <a-entity
-          id="mouseCursor"
-          bind__raycaster="enabled: !inVR"
-          cursor="rayOrigin: mouse"
-          raycaster="objects: [raycastable]"
-        ></a-entity>
-        <a-entity
-          id="mouseCursorMesh"
-          mixin="cursorMesh"
-          cursor-mesh="cursorEl: #mouseCursor"
-          bind__cursor-mesh="active: menuActive"
-          render-order="cursor"
-        ></a-entity>
-
-        <a-entity
-          tail="target: #leftstar"
-          render-order="weapon"
-          bind__visible="isPlaying && gameMode === 'ride'"
-        ></a-entity>
-        <a-entity
-          tail="target: #rightstar"
-          render-order="weapon"
-          bind__visible="isPlaying && gameMode === 'ride'"
-        ></a-entity>
-      </a-entity>
-    </a-entity>
-
-    <!-- Руки игрока с поддержкой захвата -->
-    <!--    <a-entity-->
-    <!--      id="camera"-->
-    <!--      camera-->
-    <!--      position="0 1.6 0"-->
-    <!--      look-controls="pointerLockEnabled: true"-->
-    <!--      wasd-controls="acceleration:100"-->
-    <!--    >-->
-    <!--      <a-entity-->
-    <!--        id="leftHand"-->
-    <!--        geometry="primitive: sphere; radius: 0.1"-->
-    <!--        material="color: #ffcccc"-->
-    <!--        position="-0.2 1.5 -0.5"-->
-    <!--        super-hands="usePhysics: only; constraintComponentName: ammo-constraint"-->
-    <!--        raycaster="objects: .interactive"-->
-    <!--        vive-controls="hand: left"-->
-    <!--        oculus-touch-controls="hand: left"-->
-    <!--        grabbable-->
-    <!--        stretchable-->
-    <!--        draggable-->
-    <!--        visible="true"-->
-    <!--      ></a-entity>-->
-    <!--      <a-entity-->
-    <!--        id="rightHand"-->
-    <!--        geometry="primitive: sphere; radius: 0.1"-->
-    <!--        material="color: #ffcccc"-->
-    <!--        position="0.2 1.5 -0.5"-->
-    <!--        super-hands="usePhysics: only; constraintComponentName: ammo-constraint"-->
-    <!--        raycaster="objects: .interactive"-->
-    <!--        vive-controls="hand: right"-->
-    <!--        oculus-touch-controls="hand: right"-->
-    <!--        grabbable-->
-    <!--        stretchable-->
-    <!--        draggable-->
-    <!--        visible="true"-->
-    <!--      ></a-entity>-->
-    <!--    </a-entity>-->
+    <!-- Ваши 3D-объекты -->
+    <a-box position="0 2 -5" color="red" class="clickable"></a-box>
   </a-scene>
 </template>
 
