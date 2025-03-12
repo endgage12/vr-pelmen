07918211792bf6e0e4fa31e6a1f571424690274a
alexandr <endgage12@gmail.com>
2025-03-12 22:03:22 +0300

    for test purposes

--- frontend/src/views/Storybook.vue
+++ frontend/src/views/Storybook.vue
@@ -94,7 +94,7 @@ const onThumbstickMoved = (e: any) => {
 
   // Вычисляем смещение для движения вперед/назад и в стороны
   const forwardMovement = direction.clone().multiplyScalar(e.detail.y * movementSpeed)
-  const sidewaysMovement = right.clone().multiplyScalar(e.detail.x * movementSpeed)
+  const sidewaysMovement = right.clone().multiplyScalar(-e.detail.x * movementSpeed)
 
   // Обновляем позицию rig
   rig.value.object3D.position.add(forwardMovement).add(sidewaysMovement)
