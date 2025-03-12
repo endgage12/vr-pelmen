b2d2297fdee9559851aa160071241ebdb26a6f55
alexandr <endgage12@gmail.com>
2025-03-12 22:14:24 +0300

    for test purposes

--- frontend/src/views/Storybook.vue
+++ frontend/src/views/Storybook.vue
@@ -73,24 +73,19 @@ const onThumbstickRotation = (e: any) => {
 const onThumbstickMoved = (e: any) => {
   const movementSpeed = 0.1
 
-  // Получаем камеру внутри rig
   const cameraEl = rig.value.querySelector('[camera]')
   const direction = new THREE.Vector3()
   cameraEl.object3D.getWorldDirection(direction)
 
-  // Обнуляем вертикальную компоненту и нормализуем вектор
   direction.y = 0
   direction.normalize()
 
-  // Получаем вектор вправо через векторное произведение с вектором вверх
   const up = new THREE.Vector3(0, 1, 0)
   const right = new THREE.Vector3().crossVectors(direction, up).normalize()
 
-  // Вычисляем смещения
   const forwardMovement = direction.clone().multiplyScalar(e.detail.y * movementSpeed)
   const sidewaysMovement = right.clone().multiplyScalar(-e.detail.x * movementSpeed)
 
-  // Обновляем позицию rig
   rig.value.object3D.position.add(forwardMovement).add(sidewaysMovement)
 }
 </script>
