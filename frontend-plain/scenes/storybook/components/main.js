3b916405eba3dbb4c6c80650b4aeee83ee741f56
alexandr <endgage12@gmail.com>
2025-03-13 16:20:49 +0300

    for test purposes

--- frontend-plain/scenes/storybook/components/main.js
+++ frontend-plain/scenes/storybook/components/main.js
@@ -1,6 +1,35 @@
-import { onThumbstickMoved, onThumbstickRotation } from "../../../utils/thumbstick.js";
-
 const sceneRef = document.querySelector('#sceneRef')
+const rig = document.querySelector('#rig')
+const vrLogger = document.querySelector('#vrLogger')
+
+const onThumbstickMoved = (e) => {
+    const movementSpeed = 0.1
+
+    const cameraEl = rig.querySelector('[camera]')
+    const direction = new THREE.Vector3()
+    cameraEl.object3D.getWorldDirection(direction)
+
+    direction.y = 0
+    direction.normalize()
+
+    const up = new THREE.Vector3(0, 1, 0)
+    const right = new THREE.Vector3().crossVectors(direction, up).normalize()
+
+    const forwardMovement = direction.clone().multiplyScalar(e.detail.y * movementSpeed)
+    const sidewaysMovement = right.clone().multiplyScalar(-e.detail.x * movementSpeed)
+
+    rig.object3D.position.add(forwardMovement).add(sidewaysMovement)
+}
+
+const onThumbstickRotation = (e) => {
+    vrLogger.setAttribute('value', JSON.stringify(e.detail))
+    const vectorX = -e.detail.x
+
+    const prevRotation = rig.getAttribute('rotation')
+    const nextRotation = `${prevRotation.x} ${vectorX + prevRotation.y} ${prevRotation.z}`
+    rig.setAttribute('rotation', nextRotation)
+}
+
 sceneRef.addEventListener('loaded', () => {
     console.log('scene loaded')
     const leftHand = sceneRef.querySelector('#leftHand')
