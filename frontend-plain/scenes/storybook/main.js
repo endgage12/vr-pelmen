d7d768f6c5bf54264198a49dfca6ce7cad53480c
alexandr <endgage12@gmail.com>
2025-03-13 18:52:13 +0300

    for test purposes

--- frontend-plain/scenes/storybook/main.js
+++ frontend-plain/scenes/storybook/main.js
@@ -23,7 +23,7 @@ const onThumbstickMoved = (e) => {
 
 const onThumbstickRotation = (e) => {
     vrLogger.setAttribute('value', JSON.stringify(e.detail))
-    const vectorX = -e.detail.x * 1.5
+    const vectorX = -e.detail.x * 3
 
     const prevRotation = rig.getAttribute('rotation')
     const nextRotation = `${prevRotation.x} ${vectorX + prevRotation.y} ${prevRotation.z}`
